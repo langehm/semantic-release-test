a7047c32eb4a8d3ed9c0c459aa2466a569e19359
langehm <langehm@users.noreply.github.com>
2024-11-06 21:19:46 +0100

    :locK: configured semantic release

---
+++ .releaserc.js
@@ -0,0 +1,46 @@
+const { promisify } = require('util')
+const dateFormat = require('dateformat')
+const { join } = require("node:path");
+const readFileAsync = promisify(require('fs').readFile)
+
+// Given a `const` variable `TEMPLATE_DIR` which points to "<semantic-release-gitmoji>/lib/assets/templates"
+const TEMPLATE_DIR = "node_modules/semantic-release-gitmoji/lib/assets/templates";
+// the *.hbs template and partials should be passed as strings of contents
+const template = readFileAsync(join(TEMPLATE_DIR, 'default-template.hbs'))
+const commitTemplate = readFileAsync(join(TEMPLATE_DIR, 'commit-template.hbs'))
+
+module.exports = {
+  plugins: [
+    [
+      'semantic-release-gitmoji', {
+      releaseRules: {
+        major: [ ':boom:' ],
+        minor: [ ':sparkles:' ],
+        patch: [
+          ':bug:',
+          ':ambulance:',
+          ':lock:'
+        ]
+      },
+      releaseNotes: {
+        template,
+        partials: { commitTemplate },
+        helpers: {
+          datetime: function (format = 'UTC:yyyy-mm-dd') {
+            return dateFormat(new Date(), format)
+          }
+        },
+        issueResolution: {
+          template: '{baseUrl}/{owner}/{repo}/issues/{ref}',
+          baseUrl: 'https://github.com',
+          source: 'github.com',
+          removeFromCommit: false,
+          regex: /#\d+/g
+        }
+      }
+    }
+    ],
+    '@semantic-release/github',
+    '@semantic-release/npm'
+  ]
+}
